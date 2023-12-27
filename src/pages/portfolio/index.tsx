2dd9f84984e185ea3e1c0351a59106b8ddc774d1
heyjin07 <yhhj0723@naver.com>
2023-12-27 15:29:17 +0900

    에러 수정

--- src/pages/portfolio/index.tsx
+++ src/pages/portfolio/index.tsx
@@ -1,27 +1,63 @@
-import Card from '../component/Card'
+import styles from '@/styles/Portfolio.module.css'
 import lists from '@/db/portfolio.json'
-
-interface portfolio {
-	image: string
-	subject: string
-	type: string
-	introduce: string[]
-	frontend: string
-	backend: string
-	tools: string
-	android: string
-	ios: string
-	web: string
-}
 
 const Portfolio = () => {
 	return (
 		<section id="portfolio" className="portfolio">
-			{/* <div className="linkbox cf"></div> */}
 			<div className="pf_wrapper">
-				{lists.pf.map((pf: portfolio, i) => (
+				{lists.pf.map((pf, i) => (
 					<div key={i}>
-						<Card portfolio={pf} />
+						<article className={styles.pf}>
+							<div className={styles.left_con}>
+								<img src={pf.image} alt={pf.subject} />
+							</div>
+							<div className={styles.right_con}>
+								<div className={styles.table_wrap}>
+									<table>
+										<colgroup>
+											<col width="30%" />
+											<col width="*" />
+										</colgroup>
+										<thead>
+											<tr>
+												<th>구분</th>
+												<th>내용</th>
+											</tr>
+										</thead>
+										<tbody>
+											<tr>
+												<th>프로젝트명</th>
+												<td>{pf.subject}</td>
+											</tr>
+											<tr>
+												<th>소개</th>
+												<td>
+													<ul>{pf.introduce && pf.introduce.map((list, i) => <li key={i}>{list}</li>)}</ul>
+												</td>
+											</tr>
+											<tr>
+												<th>Frontend</th>
+												<td>{pf.frontend}</td>
+											</tr>
+											{pf.backend && (
+												<tr>
+													<th>Brontend</th>
+													<td>{pf.backend}</td>
+												</tr>
+											)}
+
+											<tr>
+												<th>사용 툴</th>
+												<td>{pf.tools}</td>
+											</tr>
+										</tbody>
+									</table>
+								</div>
+								{/* {pf.android && <Link href={pf.android}>Android</Link>}
+					{pf.ios && <Link href={pf.ios}>IOS</Link>}
+					{pf.web && <Link href={pf.web}>web</Link>} */}
+							</div>
+						</article>
 					</div>
 				))}
 			</div>
